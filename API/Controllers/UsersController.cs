47384b0e3cbc6ced9b1091c2c1d331267c833149
Ivan CM <56263882+Ivan21A7x@users.noreply.github.com>
2023-09-04 08:07:14 -0600

    Seccion02 - Commit inicial API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController:ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return _context.Users.Find(id);
+    }
+
+}
