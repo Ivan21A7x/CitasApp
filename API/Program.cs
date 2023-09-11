c108c29db91adc7f595f9ade85ab732f8ed88b62
Ivan CM <56263882+Ivan21A7x@users.noreply.github.com>
2023-09-11 07:07:05 -0600

    Seccion03 - Commit inicial SPA

--- API/Program.cs
+++ API/Program.cs
@@ -7,18 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>
-(opt => 
+(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithOrigins("http://localhost:4200"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
- 
+
 app.MapControllers();
- 
+
 app.Run();
- 
