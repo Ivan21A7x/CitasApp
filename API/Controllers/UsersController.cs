89835369c40e70c6932becf3bdedded18e5c5554
Ivan CM <56263882+Ivan21A7x@users.noreply.github.com>
2023-11-16 08:35:49 -0600

    Seccion09 - Final commit - Added ngx gallery

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,8 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-[Authorize]
-public class UsersController : BaseApiController
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
@@ -21,18 +22,14 @@ public class UsersController : BaseApiController
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await _userRepository.GetUsersAsync();
-        var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-
-        return Ok(usersToReturn);
+        var user = await _userRepository.GetMembersAsync();
+        return Ok(user);
     }
 
     [HttpGet("{username}")]
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await _userRepository.GetUserByUsernameAsync(username);
-
-        return _mapper.Map<MemberDto>(user);
+        return await _userRepository.GetMemberAsync(username);
     }
 
 }
